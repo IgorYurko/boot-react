3de926a58cc40ef0c2f06e2026c89c67788a8d59
igor.y <igor.yurko.mail@gmail.com>
2017-03-14 14:56:15 +0200

    boot and react demo

---
+++ src/main/java/com/greglturnquist/payroll/EmployeeRepository.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.payroll;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+	
+	@RestResource(path = "all", rel = "all")
+	@Query("select e from Employee e")
+	List<Employee> all();
+	
+}
+// end::code[]
